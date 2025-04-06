10f844789933049bcff3bbdfd7cf989e33d2aab9
kvsiv1 <kvsiv1@gmail.com>
2025-04-06 21:39:40 +0200

    Changed spinlocks to mutexes

--- main.cpp
+++ main.cpp
@@ -8,29 +8,11 @@ int NUM_PHILOSOPHERS;
 // critical section - before philosopher eats, two next to him have to think (picking up and putting down forks)
 // thread represents a philosopher
 
-// lock using atomic bool to prevent race condition - two philosophers picking up the same fork
-struct Spinlock {
-    std::atomic<bool> state; // true = taken
-
-    Spinlock() { 
-        state.store(false); 
-    }
-
-    void pick_up() {
-        // true = fork taken
-        // keeps being taken until it's put down
-        while (state.exchange(true, std::memory_order_acquire)); 
-    }
-
-    void put_down() { 
-        state.store(false, std::memory_order_release); 
-    }
-};
-
-Spinlock* forks;
+pthread_mutex_t* forks;
 
 void* philosopher(void* arg) {
     int id = *(int*)arg; // dereference to get id
+    delete (int*)arg; // delete id
     int left = id;
     int right = (id + 1) % NUM_PHILOSOPHERS; // mod for N-1 case, i.e. N=5, 4's right fork is 0
     
@@ -41,11 +23,11 @@ void* philosopher(void* arg) {
         // Asymetric solution preventing deadlock - odd philosophers pick left fork first, even pick right first
         // deadlock - all philosophers picking left/right fork at the same time
         if (id % 2 == 0) {
-            forks[right].pick_up(); // Pick up right fork
-            forks[left].pick_up();  // Pick up left fork
+            pthread_mutex_lock(&forks[right]);
+            pthread_mutex_lock(&forks[left]);
         } else {
-            forks[left].pick_up();  // Pick up left fork
-            forks[right].pick_up(); // Pick up right fork
+            pthread_mutex_lock(&forks[left]);
+            pthread_mutex_lock(&forks[right]);
         }
 
         printf("Philosopher %d is eating...\n", id);
@@ -53,9 +35,8 @@ void* philosopher(void* arg) {
 
         printf("Philosopher %d is done eating.\n", id);
 
-        // Put down forks
-        forks[left].put_down();
-        forks[right].put_down();
+        pthread_mutex_unlock(&forks[left]);
+        pthread_mutex_unlock(&forks[right]);
     }
     return nullptr;
 }
@@ -68,18 +49,20 @@ int main(int argc, char* argv[]) {
     NUM_PHILOSOPHERS = std::stoi(argv[1]); // convert command line arg to int
 
     pthread_t* philosophers = new pthread_t[NUM_PHILOSOPHERS]; // array of philosopher threads
-    int* ids = new int[NUM_PHILOSOPHERS]; // philosopher's ids
 
-    forks = new Spinlock[NUM_PHILOSOPHERS]; // array of forks informing which ones are locked
+    forks = new pthread_mutex_t[NUM_PHILOSOPHERS]; // array of forks
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_mutex_init(&forks[i], nullptr);
+    }
 
     // create philosopher threads
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        ids[i] = i;
+        int* id = new int(i); // dynamic id array to prevent race conditions
         // create a new thread for every philosopher in pthread_t* philosophers array. 
         // nullptr default attributes are used. 
         // thread will execute philosopher function.
         // &ids[i] allows thread to know which philosopher it represents
-        pthread_create(&philosophers[i], nullptr, philosopher, &ids[i]);
+        pthread_create(&philosophers[i], nullptr, philosopher, id);
     }
 
     // join threads
@@ -87,8 +70,12 @@ int main(int argc, char* argv[]) {
         pthread_join(philosophers[i], nullptr);
     }
 
+    // cleanup
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_mutex_destroy(&forks[i]);
+    }
+
     delete[] philosophers;
-    delete[] ids;
     delete[] forks;
 
     return 0;
